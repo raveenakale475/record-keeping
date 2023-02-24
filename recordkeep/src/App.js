1aae55a331995ae26af18d5f8d79b2968f4e7a36
raveena <raveenar.kale@gmail.com>
2023-02-24 16:57:57 +0530

    updated

--- recordkeep/src/App.js
+++ recordkeep/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Stack from '@mui/material/Stack';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <Button variant="contained" color="success">
+        Add
+      </Button>
+      </div>
     </div>
   );
 }
