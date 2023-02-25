4bc7cbe13fd872424663f7f415d4eb8c891c8305
raveena <raveenar.kale@gmail.com>
2023-02-26 01:50:41 +0530

    added changes

--- recordkeep/src/App.js
+++ recordkeep/src/App.js
@@ -2,18 +2,63 @@ import "./App.css";
 import Header from "./components/Header";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import Stack from '@mui/material/Stack';
+import Stack from "@mui/material/Stack";
+import AddIcon from "@mui/icons-material/Add";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [data, setData] = useState([]);
+
+  const addData = () => {
+    setData([
+      ...data,
+      {
+        name,
+        email,
+      },
+    ]);
+    setName("");
+    setEmail("");
+  };
+
   return (
     <div className="App">
       <Header />
-      <div>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <Button variant="contained" color="success">
-        Add
-      </Button>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: "20px",
+        }}
+      >
+        {/* form section */}
+        <Stack direction="row" spacing={2}>
+          <TextField
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            id="outlined-basic"
+            label="name"
+            variant="outlined"
+          />
+          <TextField
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            id="outlined-basic"
+            label="email"
+            variant="outlined"
+          />
+          <Button onClick={addData} variant="contained" color="error">
+            <AddIcon />
+          </Button>
+        </Stack>
+      </div>
+
+      {/* data section */}
+      <div className="data">
+       
       </div>
     </div>
   );
---
+++ recordkeep/src/components/Fields.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Fields = () => {
+  return (
+    <div className="data_val">
+      <h4>Name</h4>
+      <h4>Email</h4>
+      <h4>Remove</h4>
+    </div>
+  );
+};
+
+export default Fields;
