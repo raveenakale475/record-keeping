8ca1c91e3bec659cf6cb782be7b83462dae6e8ea
raveena <raveenar.kale@gmail.com>
2023-02-27 03:09:08 +0530

    final changes have been made to it

--- recordkeep/src/App.js
+++ recordkeep/src/App.js
@@ -5,22 +5,21 @@ import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import AddIcon from "@mui/icons-material/Add";
 import { useState } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [form, setForm] = useState({});
   const [data, setData] = useState([]);
 
   const addData = () => {
-    setData([
-      ...data,
-      {
-        name,
-        email,
-      },
-    ]);
-    setName("");
-    setEmail("");
+    setData([...data, form]);
+    setForm({ form });
+  };
+
+  const removeItem = (index) => {
+    let arr = data;
+    arr.splice(index, 1);
+    setData([...arr]);
   };
 
   return (
@@ -37,20 +36,22 @@ function App() {
         {/* form section */}
         <Stack direction="row" spacing={2}>
           <TextField
-            value={name}
-            onChange={(event) => setName(event.target.value)}
+            value={form.name}
+            onChange={(event) => setForm({ ...form, name: event.target.value })}
             id="outlined-basic"
             label="name"
             variant="outlined"
           />
           <TextField
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
+            value={form.email}
+            onChange={(event) =>
+              setForm({ ...form, email: event.target.value })
+            }
             id="outlined-basic"
             label="email"
             variant="outlined"
           />
-          <Button onClick={addData} variant="contained" color="error">
+          <Button onClick={addData} variant="contained" color="success">
             <AddIcon />
           </Button>
         </Stack>
@@ -58,7 +59,28 @@ function App() {
 
       {/* data section */}
       <div className="data">
-       
+        <div className="data_val">
+          <h4>Name</h4>
+          <h4>Email</h4>
+          <h4>Remove</h4>
+        </div>
+        {data.map((element, index) => {
+          return (
+            <div kay={index} className="data_val">
+              <h4>{element.name}</h4>
+              <h4>{element.email}</h4>
+              <Stack>
+                <Button
+                  onClick={() => removeItem(index)}
+                  variant="contained"
+                  color="error"
+                >
+                  <DeleteIcon />
+                </Button>
+              </Stack>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- recordkeep/src/components/Fields.js
+++ recordkeep/src/components/Fields.js
@@ -1,11 +1,18 @@
+import { Button } from "@mui/material";
 import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Stack } from "@mui/system";
 
-const Fields = () => {
+const Fields = ({ name, email, index }) => {
   return (
     <div className="data_val">
-      <h4>Name</h4>
-      <h4>Email</h4>
-      <h4>Remove</h4>
+      <h4>{name}</h4>
+      <h4>{email}</h4>
+      <Stack>
+        <Button variant="contained" color="error">
+          <DeleteIcon />
+        </Button>
+      </Stack>
     </div>
   );
 };
