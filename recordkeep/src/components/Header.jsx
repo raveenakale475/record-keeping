31f3cb7413e215d996dec3b9be985c5bc0449182
raveena <raveenar.kale@gmail.com>
2023-04-20 23:38:22 +0530

    some changes have been added to ui

--- recordkeep/src/components/Header.jsx
+++ recordkeep/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="header">
-      <h1> Record Keeping</h1>
+      <h1> Record Keeping Application</h1>
     </div>
   );
 };
